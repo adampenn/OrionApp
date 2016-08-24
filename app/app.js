59436be73af1f1423968236a34149c73a7ad4f19
Adam J. Penn <madannep@gmail.com>
2016-08-24 12:02:27 -0700

    updating files to work on local system

--- app/app.js
+++ app/app.js
@@ -1,5 +1,11 @@
 app = angular.module('orionApp', ['ui-view']);
 
-app.controller('OrionAppController', ['$scope', function($scope) {
-
+app.controller('OrionAppController', ['$scope', '$stateProvider', '$urlRouterProvider', function($scope, $stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('news');
+  $stateProvider
+    .state('news', {
+      url: '/news'
+      templateUrl: 'public/partials/news.html'
+    })
+    .state('news'
 }]);
